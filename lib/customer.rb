94d59210e61e2831c8439c738273afebf46e4dcb
Sean <skabe89@gmail.com>
2020-11-23 23:45:30 -0500

    commit tree ccd5d093f32fda876b01799ab4e4b5ab82e7f87a

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
+    attr_accessor :name, :age
+    @@all = []
   
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        meal = Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select{|i| i.customer == self}
+    end
+
+    def waiters
+        meals.map{|i| i.customer == self ? i.waiter : nil}
+    end
 end
